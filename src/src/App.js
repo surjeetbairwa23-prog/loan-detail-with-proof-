e753e62a81f7b57914fa6023ef3398067f051919
surjeetbairwa23-prog <surjeetbairwa23@gmail.com>
2025-10-31 19:53:31 +0530

    Create App.js

---
+++ src/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoanForm from './LoanForm';
+import './App.css'; // आप CSS जोड़ सकते हैं
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Loan Management System</h1>
+      <LoanForm />
+    </div>
+  );
+}
+
+export default App;
